61a813e48f4983673df9f9a29c8ba1654169d5d3
Jordi Catala <jordicatalaamat@gmail.com>
2025-01-09 11:45:34 -0600

    refactor: remove purchase, payment, and user-related features

--- features/bento/BentoStore.tsx
+++
@@ -1,9 +0,0 @@
-import { createUseStore } from '@tamagui/use-store'
-
-class BentoStore {
-  heroVisible = true
-  heroHeight = 800
-  disableTint = true
-}
-
-export const useBentoStore = createUseStore(BentoStore)
--- features/bento/types.ts
+++
@@ -1,5 +0,0 @@
-import type { getProductsForServerSideRendering } from '../site/purchase/server-helpers'
-
-export type ProComponentsProps = Awaited<
-  ReturnType<typeof getProductsForServerSideRendering>
->
